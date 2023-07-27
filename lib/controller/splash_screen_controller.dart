0c32a0284be892a300567191a0b66e511890b7b2
Mkhaleefah <khaleefah.m@fssglobal.in>
2023-07-27 18:39:15 +0530

    splash screen changes

--- lib/controller/splash_screen_controller.dart
+++ lib/controller/splash_screen_controller.dart
@@ -1,13 +1,235 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:netflix_uiapp/pages/login_page.dart';
 import 'package:netflix_uiapp/pages/slidepage1.dart';
+import 'package:netflix_uiapp/pages/slidepage2.dart';
+import 'package:onboarding/onboarding.dart';
 
 class SplashScreenController extends GetxController {
+  late Material materialButton;
+  late int index;
+  final onboardingPagesList = [
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: background,
+          //color: Colors.black87,
+          border: Border.all(
+            width: 0.0,
+            color: background,
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(9),
+              // padding: const EdgeInsets.symmetric(
+              //   horizontal: 45.0,
+              //   vertical: 90.0,
+              // ),
+              child: Image.asset(
+                'assets/images/slidepage1.png',
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Center(
+                child: Text(
+                  'Watch on\nany device',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    //fontStyle: FontStyle.normal
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Align(
+              // alignment: Alignment.centerLeft,
+              child: Text(
+                'Stream on Your phone,tablet,',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontStyle: FontStyle.normal),
+                // textAlign: TextAlign.left,
+              ),
+            ),
+            Text(
+              'laptop,and TV without paying more..',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal),
+              // textAlign: TextAlign.left,
+            ),
+          ],
+        ),
+      ),
+    ),
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: background,
+          border: Border.all(
+            //  width: 0.0,
+            color: background,
+          ),
+        ),
+        child:
+            //  SingleChildScrollView(
+            //   controller: ScrollController(),
+            //  child:
+            Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(12),
+              // padding: const EdgeInsets.symmetric(
+              //   horizontal: 45.0,
+              //   vertical: 90.0,
+              // ),
+              child: Image.asset(
+                'assets/images/slidepage2.png',
+                // color: pageImageColor
+              ),
+            ),
+            Text(
+              'Download and go',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 35),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Align(
+              //alignment: Alignment.centerLeft,
+              child: Text(
+                'Save your data,watch offline on a\n      plane,train,or submarine...',
+                style: pageInfoStyle,
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    //  ),
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          // border: Border.all(
+          //   width: 0.0,
+          //   color: background,
+          // ),
+        ),
+        child:
+            // SingleChildScrollView(
+            //   controller: ScrollController(),
+            //   child:
+            Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(12),
+              // padding: const EdgeInsets.symmetric(
+              //   horizontal: 45.0,
+              //   vertical: 90.0,
+              // ),
+              child: Image.asset(
+                'assets/images/sildepage3 .png',
+                //  color: pageImageColor
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 45.0),
+              child: Align(
+                // alignment: Alignment.centerLeft,
+                child: Text(
+                  'No Pesky contracts',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                  //  style: pageTitleStyle,
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Align(
+             // alignment: Alignment.centerLeft,
+              child: Text(
+                'Join today,cancel anytime',
+                style: pageInfoStyle,
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    // ),
+  ];
+  Material skipButton({void Function(int)? setIndex}) {
+    return Material(
+     // borderRadius: defaultSkipButtonBorderRadius,
+      color: defaultSkipButtonColor,
+      child: InkWell(
+        borderRadius: defaultSkipButtonBorderRadius,
+        onTap: () {
+          if (setIndex != null) {
+            index = 2;
+            setIndex(2);
+          }
+        },
+        child: const Padding(
+          padding: defaultSkipButtonPadding,
+          child: Text(
+            'Skip',
+            style: defaultSkipButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material get signupButton {
+    return Material(
+      borderRadius: defaultProceedButtonBorderRadius,
+      color: defaultProceedButtonColor,
+      child: InkWell(
+        borderRadius: defaultProceedButtonBorderRadius,
+        onTap: () {},
+        child: const Padding(
+          padding: defaultProceedButtonPadding,
+          child: Text(
+            'Login',
+            style: defaultProceedButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   void onInit() {
     // TODO: implement onInit
     Future.delayed(Duration(seconds: 5))
-        .then((value) => Get.off(() => SlidePage1()));
+        .then((value) => Get.off(() => SlidePage2()));
     super.onInit();
+    materialButton = skipButton();
+    // materialButton = LoginPage();
+    index = 0;
   }
-  
 }
--- lib/pages/slidepage2.dart
+++ lib/pages/slidepage2.dart
@@ -1,263 +1,11 @@
-// import 'package:flutter/material.dart';
-
-// class SlidePage2 extends StatelessWidget {
-//   const SlidePage2({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         child: Text(
-//           'jfhgjhfjfjj',
-//           style: TextStyle(color: Colors.amber, fontSize: 30),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:netflix_uiapp/controller/splash_screen_controller.dart';
 import 'package:onboarding/onboarding.dart';
 
-class SlidePage2 extends StatefulWidget {
-  const SlidePage2({Key? key}) : super(key: key);
-
-  @override
-  State<SlidePage2> createState() => _SlidePage2();
-}
-
-class _SlidePage2 extends State<SlidePage2> {
-  late Material materialButton;
-  late int index;
-  final onboardingPagesList = [
-    PageModel(
-      widget: DecoratedBox(
-        decoration: BoxDecoration(
-          color: background,
-          // border: Border.all(
-          //   width: 0.0,
-          //   color: background,
-          // ),
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 45.0,
-                vertical: 90.0,
-              ),
-              child: Image.asset(
-                'assets/images/slidepage1.png',
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text(
-                      'Watch on ',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 35,
-                          fontStyle: FontStyle.normal),
-                      textAlign: TextAlign.left,
-                    ),
-                    Text(
-                      'any device ',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 35,
-                          fontStyle: FontStyle.normal),
-                      textAlign: TextAlign.left,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Stream on Your phone,tablet,',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontStyle: FontStyle.normal),
-                  // textAlign: TextAlign.left,
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'laptop,and TV without paying more.',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontStyle: FontStyle.normal),
-                  // textAlign: TextAlign.left,
-                ),
-              ),
-            ),
-
-            // ElevatedButton(onPressed: (){},
-            // child: Text('sign in'))
-          ],
-        ),
-      ),
-    ),
-    PageModel(
-      widget: DecoratedBox(
-        decoration: BoxDecoration(
-          color: background,
-          border: Border.all(
-            width: 0.0,
-            color: background,
-          ),
-        ),
-        child: SingleChildScrollView(
-          controller: ScrollController(),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 45.0,
-                  vertical: 90.0,
-                ),
-                child: Image.asset('assets/images/slidepage2.png',
-                    color: pageImageColor),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 45.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'CHANGE AND RISE',
-                    style: pageTitleStyle,
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Give others access to any file or folders you choose',
-                    style: pageInfoStyle,
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-    PageModel(
-      widget: DecoratedBox(
-        decoration: BoxDecoration(
-          color: background,
-          border: Border.all(
-            width: 0.0,
-            color: background,
-          ),
-        ),
-        child: SingleChildScrollView(
-          controller: ScrollController(),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 45.0,
-                  vertical: 90.0,
-                ),
-                child: Image.asset('assets/images/sildepage3.png',
-                    color: pageImageColor),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 45.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'EASY ACCESS',
-                    style: pageTitleStyle,
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Reach your files anytime from any devices anywhere',
-                    style: pageInfoStyle,
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    materialButton = _skipButton();
-    index = 0;
-  }
-
-  Material _skipButton({void Function(int)? setIndex}) {
-    return Material(
-      borderRadius: defaultSkipButtonBorderRadius,
-      color: defaultSkipButtonColor,
-      child: InkWell(
-        borderRadius: defaultSkipButtonBorderRadius,
-        onTap: () {
-          if (setIndex != null) {
-            index = 2;
-            setIndex(2);
-          }
-        },
-        child: const Padding(
-          padding: defaultSkipButtonPadding,
-          child: Text(
-            'Skip',
-            style: defaultSkipButtonTextStyle,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Material get _signupButton {
-    return Material(
-      borderRadius: defaultProceedButtonBorderRadius,
-      color: defaultProceedButtonColor,
-      child: InkWell(
-        borderRadius: defaultProceedButtonBorderRadius,
-        onTap: () {},
-        child: const Padding(
-          padding: defaultProceedButtonPadding,
-          child: Text(
-            'Sign up',
-            style: defaultProceedButtonTextStyle,
-          ),
-        ),
-      ),
-    );
-  }
+class SlidePage2 extends StatelessWidget {
+  SplashScreenController screenController = Get.put(SplashScreenController());
 
   @override
   Widget build(BuildContext context) {
@@ -269,15 +17,51 @@ class _SlidePage2 extends State<SlidePage2> {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            'NETFLIX',
+            style: GoogleFonts.lato(
+              color: Colors.redAccent,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              // fontStyle: FontStyle.normal,
+            ),
+          ),
+          actions: [
+            Row(
+              children: [
+                Text(
+                  'Privacy',
+                  style: GoogleFonts.lato(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Help',
+                  style: GoogleFonts.lato(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
+          ],
+        ),
         body: Onboarding(
-          pages: onboardingPagesList,
+          pages: screenController.onboardingPagesList,
           onPageChange: (int pageIndex) {
-            index = pageIndex;
+            screenController.index = pageIndex;
           },
           startPageIndex: 0,
           footerBuilder: (context, dragDistance, pagesLength, setIndex) {
             return DecoratedBox(
               decoration: BoxDecoration(
+                // color: Colors.blue,
                 color: background,
                 border: Border.all(
                   width: 0.0,
@@ -285,28 +69,56 @@ class _SlidePage2 extends State<SlidePage2> {
                 ),
               ),
               child: ColoredBox(
+                //  color: Colors.red,
                 color: background,
-                child: Padding(
-                  padding: const EdgeInsets.all(45.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CustomIndicator(
-                        netDragPercent: dragDistance,
-                        pagesLength: pagesLength,
-                        indicator: Indicator(
-                          indicatorDesign: IndicatorDesign.line(
-                            lineDesign: LineDesign(
-                              lineType: DesignType.line_uniform,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CustomIndicator(
+                            netDragPercent: dragDistance,
+                            pagesLength: pagesLength,
+                            indicator: Indicator(
+                              indicatorDesign: IndicatorDesign.polygon(
+                                polygonDesign: PolygonDesign(
+                                  polygon: DesignType.polygon_circle,
+                                ),
+                              ),
+                            )
+
+                            // indicator: Indicator(
+                            //   indicatorDesign: IndicatorDesign.line(
+                            //     lineDesign: LineDesign(
+                            //       lineType: DesignType.line_uniform,
+                            //     ),
+                            //   ),
+                            // ),
                             ),
+                        screenController.index == pagesLength - 1
+                            ? screenController.signupButton
+                            : screenController.skipButton(setIndex: setIndex),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: Size(300, 50),
+                            shape: BeveledRectangleBorder(),
+                            backgroundColor: Colors.green,
+                            elevation: 0.0),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
                           ),
-                        ),
-                      ),
-                      index == pagesLength - 1
-                          ? _signupButton
-                          : _skipButton(setIndex: setIndex)
-                    ],
-                  ),
+                        )),
+                  ],
                 ),
               ),
             );
